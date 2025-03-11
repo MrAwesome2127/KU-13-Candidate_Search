7d06ac40eb5db07ec44dbc1ebc958618eafd7d95
Tim Velasquez <Timmyv1031@gmail.com>
2025-03-11 00:51:39 -0500

    added allowedHosts

--- Develop/vite.config.ts
+++ Develop/vite.config.ts
@@ -8,5 +8,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0', // Bind to 0.0.0.0 to allow external access
     port: 5173, // Specify the port
+    allowedHosts: ['https://ku-13-candidate-search.onrender.com'],
   },
 });
